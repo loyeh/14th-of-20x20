12c7cd49cfee3892135233b11fc55c3d19014759
loyeh <asadi.loyeh@gmail.com>
2023-04-24 13:43:10 +0430

    new card construction page added

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-document.body.style.height=window.innerHeight;
+document.body.style.height = window.innerHeight;
+const first_page = document.querySelector(".first_page");
+const new_card = document.querySelector(".new_card");
+
+function addNew() {
+  first_page.className="first_page hide"
+  new_card.className="new_card show"
+}
+
+function close_new() {
+    new_card.className="new_card hide"
+    first_page.className="first_page show"
+}
+function addCard() {}
