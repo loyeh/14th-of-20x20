78b2bb7f22baa4fb28d4b346d1d07566ff306f04
loyeh <asadi.loyeh@gmail.com>
2023-04-26 22:55:57 +0330

    final commit

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const card_number = document.getElementById("card_number");
 const myForm = document.getElementById("myForm");
 const main = document.getElementById("main");
 let cards = [];
-let displaying_card_index = 1;
+let displaying_card_index = 0;
 
 // {localStorage.setItem("cards_arr",JSON.stringify(cards))}
 // if (!localStorage.getItem("cards_arr")) {
@@ -26,37 +26,37 @@ function previous() {
 	if (displaying_card_index < 0) {
 		displaying_card_index = cards.length - 1;
 	}
-	const card_container = document.querySelector(".card_container");
-	card_container.style.left = `${displaying_card_index * -540}px`;
-	const cardsArr = document.querySelectorAll(".card");
-	cardsArr.forEach((elem) => {
-		if (elem.dataset.number != (displaying_card_index+1)) {
+	document.querySelector(".card_container").style.left = `${
+		displaying_card_index * -540
+	}px`;
+	document.querySelectorAll(".card").forEach((elem) => {
+		if (elem.dataset.number != displaying_card_index + 1) {
 			elem.style.opacity = "0";
 		} else {
 			elem.style.opacity = 1;
 		}
 	});
-  card_number.innerText = `${displaying_card_index+1}/${cards.length}`;
-		makeCards(cards);
+	card_number.innerText = `${displaying_card_index + 1}/${cards.length}`;
+	// makeCards(cards);
 }
 
 function next() {
 	displaying_card_index++;
-	if (displaying_card_index > cards.length -1) {
+	if (displaying_card_index > cards.length - 1) {
 		displaying_card_index = 0;
 	}
 	const card_container = document.querySelector(".card_container");
 	card_container.style.left = `${displaying_card_index * -540}px`;
 	const cardsArr = document.querySelectorAll(".card");
 	cardsArr.forEach((elem) => {
-		if (elem.dataset.number != (displaying_card_index+1)) {
+		if (elem.dataset.number != displaying_card_index + 1) {
 			elem.style.opacity = "0";
 		} else {
 			elem.style.opacity = 1;
 		}
 	});
-  card_number.innerText = `${displaying_card_index+1}/${cards.length}`;
-		makeCards(cards);
+	card_number.innerText = `${displaying_card_index + 1}/${cards.length}`;
+	// makeCards(cards);
 }
 
 function clear_all() {
@@ -85,7 +85,7 @@ function initCards() {
 		card_number.innerText = `1/${cards.length}`;
 		makeCards(cards);
 	}
-	console.log(cards[0]);
+	// console.log(cards[0]);
 	showCard(1);
 }
 
@@ -113,24 +113,19 @@ window.addEventListener("resize", () => {
 
 // card.addEventListener("click", flip);
 
-function flip() {
-	const card = document.getElementById("card");
-
+function flip(event) {
+const card=event.target;
 	card.classList.add("annimated");
-
+	const front = card.childNodes[2];
+	const back = card.childNodes[4];
 	// card.addEventListener("animationend", flipping);
-	setTimeout(flipping, 250);
+	setTimeout(()=>{front.classList.toggle("hide");
+	back.classList.toggle("hide");}, 250);
 	setTimeout(() => {
 		card.classList.remove("annimated");
 	}, 500);
-}
-function flipping() {
-	const front = document.getElementById("front");
-	const back = document.getElementById("back");
-	front.classList.toggle("hide");
-	back.classList.toggle("hide");
-}
-function flippingBack() {}
+console.log(card.childNodes)}
+
 myForm.addEventListener("submit", addCard);
 // console.log(card.dataset.number);
 
@@ -149,7 +144,7 @@ function makeCards(cardsArr) {
 	console.log(cardNodes);
 	const card_container = document.createElement("div");
 	card_container.className = "card_container";
-	cardNodes.forEach((cardNode) => {
+	cardNodes.forEach((cardNode,index) => {
 		card_container.append(cardNode);
 	});
 	main.append(card_container);
@@ -157,5 +152,5 @@ function makeCards(cardsArr) {
 
 function showCard(index) {
 	const cardNode = document.querySelector(`[data-number="${index}"]`);
-	console.log(cardNode.dataset.number);
+	// console.log(cardNode.dataset.number);
 }
